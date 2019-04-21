aa0dcf403fa472d1b0d25969d1ccc7c8256e9479
astrid <UO258194@uniovi.es>
2019-04-21 14:09:33 +0200

    “SDI-SW-11.1 Servicio SOAP”.

---
+++ demo/src/main/java/com/uniovi/SdixLabSoapSwApplication.java
@@ -0,0 +1,14 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SdixLabSoapSwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdixLabSoapSwApplication.class, args);
+	}
+
+}
